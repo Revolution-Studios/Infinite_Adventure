8a09a4d70e9a7644a4a610a0d881f84dfb94241d
Joshua Segui <joshua.segui.rodriguez@gmail.com>
2022-10-10 16:54:00 -0400

    quick S press no animation fix

--- Player.gd
+++ Player.gd
@@ -8,16 +8,21 @@ var velocity: Vector2 = Vector2.ZERO
 onready var ship_nose: Node2D = $Sprite/Ship_Nose
 onready var flame_exhaust: Node2D = $AnimatedSprite
 
+func _ready() -> void:
+	flame_exhaust.hide()
+
 func _physics_process(delta: float) -> void:
 	direction = (ship_nose.global_position - global_position).normalized()
 	
 	if Input.is_action_pressed("move_forward"):
 		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+		flame_exhaust.show()
 		flame_exhaust.play()
 	
 	if Input.is_action_just_released("move_forward"):
 		flame_exhaust.stop()
 		flame_exhaust.frame = 0
+		flame_exhaust.hide()
 		flame_exhaust.animation = "acceleration"
 	
 	if Input.is_action_just_released("flip_maneuver"):
