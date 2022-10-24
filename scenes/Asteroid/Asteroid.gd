fd5f9e33c01451fdc5a3a25baded5ee9254286ba
A.correa <Mokimbo1@gmail.com>
2022-10-24 13:18:27 -0400

    random asteroids load on spawn

--- scenes/Asteroid/Asteroid.gd
+++ scenes/Asteroid/Asteroid.gd
@@ -1,7 +1,9 @@
 extends RigidBody2D
 
 onready var animated_sprite = $AnimatedSprite
+onready var asteroid_type = $AnimatedSprite.frames.get_animation_names()
 
 func _ready() -> void:
+	randomize()
 	animated_sprite.frame = rand_range(0,22)
-	animated_sprite.play("Asteroid1")
+	animated_sprite.play(asteroid_type[randi() % asteroid_type.size()])
