13080c9106b668e6367e5c00cfccc419a5b2502b
A.correa <Mokimbo1@gmail.com>
2022-10-24 14:58:54 -0400

    changes to ship/asteroid  behavior

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
+var hull_health = 100
 var acceleration: int = 250
 var max_speed: int = 500
 var rotation_speed: int = 150
+var inertia = 25
 var direction: Vector2
 var velocity: Vector2 = Vector2.ZERO
 onready var ship_nose: Node2D = $Sprite/Ship_Nose
@@ -34,8 +36,17 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("rotate_right"):
 		rotation_degrees+= rotation_speed * delta
 		
-	velocity = move_and_slide(velocity)
-	
+	velocity = move_and_slide(velocity, Vector2(0, 0), false, 4, 0.785, false)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		print("Collided with ", collision.collider.name)
+		if collision.collider.name.begins_with("Asteroid"):
+			collision.collider.apply_central_impulse(-collision.normal * inertia)
+			if hull_health > 0:
+				hull_health = hull_health -10
+			else:
+				print("health is 0")
+				visible = false
 
 func _on_AnimatedSprite_animation_finished() -> void:
 	flame_exhaust.animation = "max-speed"
