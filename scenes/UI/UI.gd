a3de2eb39557d19601cbef5458b8060db2321601
A.correa <Mokimbo1@gmail.com>
2022-10-27 20:27:49 -0400

    added Ui health bar tracking

--- scenes/UI/UI.gd
+++ scenes/UI/UI.gd
@@ -8,9 +8,11 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	PlayerState.connect("hull_health_changed",self, "update_health")
+func update_health(val):
+	$ProgressBar.value = PlayerState.hull_health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+	#
+	
