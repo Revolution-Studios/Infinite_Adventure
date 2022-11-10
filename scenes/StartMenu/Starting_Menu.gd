d5b08fdc479a64ffa5811a48520e757d1e5da63d
Joshua Segui <joshua.segui.rodriguez@gmail.com>
2022-11-10 16:32:04 -0500

    scalable start menu with functioning start game and quit buttons

---
+++ scenes/StartMenu/Starting_Menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+onready var start_game = $VBoxContainer/Start
+const world = preload("res://scenes/World/World.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start_game.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Start_pressed() -> void:
+	get_tree().change_scene_to(world)
+
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
