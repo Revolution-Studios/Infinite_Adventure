56777aa91e8af7fab76620b1554e6723c4907704
Joshua Segui <joshua.segui.rodriguez@gmail.com>
2022-11-22 09:59:03 -0500

    set world scene to default continue processesing during a pause, set player to pause during a pause, also paused on launch

--- scenes/World/World.gd
+++ scenes/World/World.gd
@@ -1,13 +1,10 @@
 extends Node2D
 
-var new_dialog = Dialogic.start('Tutorial')
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_child(new_dialog)
+	get_tree().paused = true
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_released("Toggle_full_screen"):
 		OS.window_fullscreen = !OS.window_fullscreen
