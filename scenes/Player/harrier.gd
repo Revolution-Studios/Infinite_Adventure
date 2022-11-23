811fea5ae78119f60cb17b43ada0b63cf44f28a0
Joshua Segui <joshua.segui.rodriguez@gmail.com>
2022-11-23 14:24:17 -0500

    removed player scene from world scene, world scene now instances a unique player ship scene based on chosen pilot

---
+++ scenes/Player/harrier.gd
@@ -0,0 +1,10 @@
+extends "res://scenes/Player/Player.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+#func _process(delta: float) -> void:
+#	pass
