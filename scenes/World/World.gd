572f9ffb30bdd716607c4135ab724c788804083e
Joshua Segui <joshua.segui.rodriguez@gmail.com>
2022-11-22 19:00:28 -0500

    updated toggle full screen to use input function instead of physics process function

--- scenes/World/World.gd
+++ scenes/World/World.gd
@@ -7,8 +7,8 @@ func _ready() -> void:
 	get_tree().paused = true
 
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_released("Toggle_full_screen"):
+func _input(event) -> void:
+	if event.is_action_pressed("Toggle_full_screen"): 
 		OS.window_fullscreen = !OS.window_fullscreen
 
 
