1f43c68e504df206d79c95d63d55d9d0052551e4
A.correa <Mokimbo1@gmail.com>
2022-11-24 12:52:43 -0500

    landing request speed check

--- scenes/UI/UI.gd
+++ scenes/UI/UI.gd
@@ -9,7 +9,7 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	PlayerState.connect("hull_health_changed",self, "update_health")
-func update_health(val):
+func update_health(_val):
 	$ProgressBar.value = PlayerState.hull_health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
