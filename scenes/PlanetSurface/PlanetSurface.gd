4741e616a8c8ca04ab9c0711456fed5b5498b8fc
A.correa <Mokimbo1@gmail.com>
2022-12-23 20:17:33 -0500

    added ability to leave planet with esc

---
+++ scenes/PlanetSurface/PlanetSurface.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		GameState.scene = Constants.SceneId.World
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
