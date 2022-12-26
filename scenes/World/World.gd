5b82ffb4cb533e1f59246d67b35fbcf7a8b37ae3
Joshua Segui <joshua.segui.rodriguez@gmail.com>
2022-12-26 12:50:16 -0500

    instanced in dialogic via code in world ready function, connected dialogic signals, tutorial now only starts if player character isn't defined

--- scenes/World/World.gd
+++ scenes/World/World.gd
@@ -5,7 +5,16 @@ var player_ship = null
 var ship_name = null
 
 func _ready() -> void:
-	get_tree().paused = true
+	if GameState.player.character == null:
+		var new_dialog = Dialogic.start("Tutorial")
+		add_child(new_dialog)
+		new_dialog.connect("dialogic_signal", self, "_on_Dialog_dialogic_signal")
+		new_dialog.connect("timeline_end", self, "_on_Dialog_timeline_end")
+		new_dialog.set_pause_mode(2)
+		get_tree().paused = true
+	else: 
+		pilot.add_child(GameState.player.ship.instance())
+		pilot.global_position = Vector2(640,520)
 
 func _on_Dialog_timeline_end(_timeline_name) -> void:
 	get_tree().paused = false
@@ -14,10 +23,12 @@ func _on_Dialog_timeline_end(_timeline_name) -> void:
 func _on_Dialog_dialogic_signal(value) -> void:
 	if value == "Yam":
 		var harrier = load("res://scenes/Ships/Harrier/Harrier.tscn")
+		GameState.player.ship = harrier
 		player_ship = harrier.instance()
 		ship_name = "Harrier"
 	else:
 		var falcon = load("res://scenes/Ships/Falcon/Falcon.tscn")
+		GameState.player.ship = falcon
 		player_ship = falcon.instance()
 		ship_name = "Falcon"
 	
@@ -25,4 +36,4 @@ func _on_Dialog_dialogic_signal(value) -> void:
 	pilot.global_position = Vector2(640,520)
 	GameState.player.character = value
 	GameState.player.ship_type = ship_name
-	GameState.player.ship = pilot.get_node(ship_name)
+	
