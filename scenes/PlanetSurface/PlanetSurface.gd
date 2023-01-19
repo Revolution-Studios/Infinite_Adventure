1b020964ed0074460202dd215a4e9ceff55e62e1
A.correa <Mokimbo1@gmail.com>
2023-01-19 14:34:38 -0500

    moved physics process code to input event since its also a keypress

--- scenes/PlanetSurface/PlanetSurface.gd
+++ scenes/PlanetSurface/PlanetSurface.gd
@@ -5,17 +5,14 @@ onready var Surface_start = $CanvasLayer/PlanetPanel/surfacelocations/Bar
 func _ready() -> void:
 	Surface_start.grab_focus()
 
-func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_select"):
-		if $CanvasLayer/PlanetPanel/surfacelocations/Bar.has_focus():
-			$CanvasLayer/PlanetPanel/surfacelocations/Bar.release_focus()
-		else:
-			$CanvasLayer/PlanetPanel/surfacelocations/Bar.grab_focus()
-
-
 func _on_Leave_pressed():
 	GameState.scene = Constants.SceneId.World
 
 func _input(event):
+	if event.is_action_just_pressed("ui_select"):
+		if $CanvasLayer/PlanetPanel/surfacelocations/Bar.has_focus():
+			$CanvasLayer/PlanetPanel/surfacelocations/Bar.release_focus()
+		else:
+			$CanvasLayer/PlanetPanel/surfacelocations/Bar.grab_focus()
 	if event.is_action_pressed("take_off"):
 		GameState.scene = Constants.SceneId.World
