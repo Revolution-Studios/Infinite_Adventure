c6d6e246b8e59efc50d64b5552de4679e8e61fac
A.correa <Mokimbo1@gmail.com>
2023-02-23 19:08:15 -0500

    mars and venus now have background pictures. added support for planet surface buttons for backround changes later

--- scenes/PlanetSurface/PlanetSurface.gd
+++ scenes/PlanetSurface/PlanetSurface.gd
@@ -15,6 +15,8 @@ func _ready() -> void:
 
 func _on_Leave_pressed():
 	GameState.scene = Constants.SceneId.World
+	GameState.player.planet_id = null
+	
 
 func _input(event):
 	if event.is_action_pressed("ui_select"):
@@ -24,3 +26,52 @@ func _input(event):
 			$CanvasLayer/PlanetPanel/surfacelocations/Bar.grab_focus()
 	if event.is_action_pressed("take_off"):
 		GameState.scene = Constants.SceneId.World
+		GameState.player.planet_id = null
+
+func _on_Bar_pressed():
+	$CanvasLayer/PlanetPanel/RichTextLabel.text = planet_data.name
+	if "bar_image" in planet_data:
+		var texture = ImageTexture.new()
+		var image = Image.new()
+		image.load(planet_data.background_image)
+		texture.create_from_image(image)
+		$Backroundimage.texture = texture
+
+func _on_Missions_pressed():
+	$CanvasLayer/PlanetPanel/RichTextLabel.text = planet_data.name
+	if "jobboard_image" in planet_data:
+		var texture = ImageTexture.new()
+		var image = Image.new()
+		image.load(planet_data.background_image)
+		texture.create_from_image(image)
+		$Backroundimage.texture = texture
+
+
+func _on_Trade_Center_pressed():
+	$CanvasLayer/PlanetPanel/RichTextLabel.text = planet_data.name
+	if "storefront_image" in planet_data:
+		var texture = ImageTexture.new()
+		var image = Image.new()
+		image.load(planet_data.background_image)
+		texture.create_from_image(image)
+		$Backroundimage.texture = texture
+
+
+func _on_Outfitter_pressed():
+	$CanvasLayer/PlanetPanel/RichTextLabel.text = planet_data.name
+	if "shipparts_image" in planet_data:
+		var texture = ImageTexture.new()
+		var image = Image.new()
+		image.load(planet_data.background_image)
+		texture.create_from_image(image)
+		$Backroundimage.texture = texture
+
+
+func _on_Refuel_pressed():
+	$CanvasLayer/PlanetPanel/RichTextLabel.text = planet_data.name
+	if "background_image" in planet_data:
+		var texture = ImageTexture.new()
+		var image = Image.new()
+		image.load(planet_data.background_image)
+		texture.create_from_image(image)
+		$Backroundimage.texture = texture
