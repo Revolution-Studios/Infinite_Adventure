0903c2b745279557de0284b35d42ae85aed76eca
Aaron Larner <alarner@users.noreply.github.com>
2023-03-08 14:30:02 -0800

    Fix planet image load warning

--- scenes/Planet/Planet.gd
+++ scenes/Planet/Planet.gd
@@ -6,6 +6,4 @@ var planet_data = null
 
 func _ready():
 	if "planet_image" in planet_data:
-		var image = Image.new()
-		image.load(planet_data.planet_image)
-		$Sprite2D.texture = ImageTexture.create_from_image(image)
+		$Sprite2D.texture = load(planet_data.planet_image)
